ea7f9905dec0443c166b1cf547e752c082d1d7cb
Ivan <ivanulanovskij143@gmail.com>
2021-04-11 19:17:30 +0500

    the nav panel is made

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,13 @@
 import scss from '../css/style.scss';
-let f = 85
+
+document.querySelector('.nav__group-links').addEventListener('click' , (event) => {
+    if (event.target.classList.contains('group-links__link')) {
+        let arrWithActiveLinks = document.querySelectorAll('.group-links__link_active')
+        if (arrWithActiveLinks.length >= 1) {
+            for (let i = 0; i < arrWithActiveLinks.length; i++) {
+                arrWithActiveLinks[i].classList.remove('group-links__link_active')
+            }
+        }
+        event.target.classList.toggle('group-links__link_active')
+    }
+})
