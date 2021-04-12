718adb89e6b98b9552bcf09976d82348df8f3e88
Ivan <ivanulanovskij143@gmail.com>
2021-04-12 12:23:46 +0500

    the name of the classes was corrected and a block with recommendations was made

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 import scss from '../css/style.scss';
 
 document.querySelector('.nav__group-links').addEventListener('click' , (event) => {
-    if (event.target.classList.contains('group-links__link')) {
-        let arrWithActiveLinks = document.querySelectorAll('.group-links__link_active')
+    if (event.target.classList.contains('nav__link')) {
+        let arrWithActiveLinks = document.querySelectorAll('.nav__link_active')
         if (arrWithActiveLinks.length >= 1) {
             for (let i = 0; i < arrWithActiveLinks.length; i++) {
-                arrWithActiveLinks[i].classList.remove('group-links__link_active')
+                arrWithActiveLinks[i].classList.remove('nav__link_active')
             }
         }
-        event.target.classList.toggle('group-links__link_active')
+        event.target.classList.toggle('nav__link_active')
     }
 })
