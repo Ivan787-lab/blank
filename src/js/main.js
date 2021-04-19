e98308b671636de673a14d85ade8401e1ec45362
Ivan <ivanulanovskij143@gmail.com>
2021-04-19 12:03:24 +0500

    was done three blocks

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import scss from '../css/style.scss';
 
-document.querySelector('.nav__group-links').addEventListener('click' , (event) => {
+document.querySelector('.nav__group-links').addEventListener('click', (event) => {
     if (event.target.classList.contains('nav__link')) {
         let arrWithActiveLinks = document.querySelectorAll('.nav__link_active')
         if (arrWithActiveLinks.length >= 1) {
@@ -11,3 +11,9 @@ document.querySelector('.nav__group-links').addEventListener('click' , (event) =
         event.target.classList.toggle('nav__link_active')
     }
 })
+
+for (let i = 1; i < 16; i++) {
+    let partner = document.createElement('img')
+    partner.src = `img/partners/partner__${i}.png`
+    document.querySelector('.our-partners__images').append(partner)
+}
